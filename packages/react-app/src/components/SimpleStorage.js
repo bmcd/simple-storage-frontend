e2848c24df569ae46f3659cff1ee6aecbd6211e7
Brad McDermott <bradmcdermott@gmail.com>
2021-03-12 14:43:05 -0500

    Slightly better wait for tx behavior

--- packages/react-app/src/components/SimpleStorage.js
+++ packages/react-app/src/components/SimpleStorage.js
@@ -6,36 +6,49 @@ import { BigNumber } from '@ethersproject/bignumber'
 
 export default function SimpleStorage ({ provider }) {
 
+  const [connected, setConnected] = useState(false)
   const [currentValue, setCurrentValue] = useState()
   const [newValue, setNewValue] = useState('')
-  const [status, setStatus] = useState('READY')
+  const [pendingTx, setPendingTx] = useState()
 
   async function refresh () {
     const network = await provider.getNetwork()
     const localhostSS1 = new Contract(addresses[network.chainId], abis.simpleStorageV1, provider)
-    const storageValue = await localhostSS1.get()
-    setCurrentValue(storageValue.toNumber())
+    console.log('Connecting to contract at', addresses[network.chainId], 'on network with id:', network.chainId)
+    try {
+      await localhostSS1.deployed()
+      const storageValue = await localhostSS1.get()
+      setCurrentValue(storageValue.toNumber())
+      setConnected(true)
+    } catch (e) {
+      console.log('Could not connect to contract at', addresses[network.chainId], 'Reason:', e)
+      setConnected(false)
+    }
   }
 
   async function set (newValueString) {
-    setStatus("SENDING")
+    if(!newValueString) {
+      return
+    }
+    setPendingTx('SENDING')
+    setNewValue('')
     const network = await provider.getNetwork()
     const newValue = BigNumber.from(newValueString)
     const localhostSS1 = new Contract(addresses[network.chainId], abis.simpleStorageV1, provider)
     localhostSS1.connect(provider.getSigner()).set(newValue)
       .then((res) => {
         console.log('waiting for tx with hash', res.hash)
-        setStatus("WAITING")
+        setPendingTx(res.hash)
         return res.wait()
       })
       .then((res) => {
         console.log('tx complete', res)
-        setStatus("READY")
+        setPendingTx(undefined)
         refresh()
       })
       .catch(e => {
         console.error('error sending transaction', e)
-        setStatus("READY")
+        setPendingTx(undefined)
       })
   }
 
@@ -43,6 +56,10 @@ export default function SimpleStorage ({ provider }) {
   useEffect(() => {
     refresh()
   }, [provider])
+
+  if(!connected) {
+    return <div>Not connected to contract.</div>
+  }
 
   return <div>
     <div>
@@ -54,11 +71,12 @@ export default function SimpleStorage ({ provider }) {
     <div>
       <input type="number" value={newValue} onChange={(event) => setNewValue(event.target.value)}/>
       <Button
-        disabled={status !== 'READY'}
+        disabled={pendingTx}
         onClick={() => set(newValue)}>
-        {status === 'READY' ? 'Set' : status === 'SENDING' ? 'Sending...' : 'Waiting...'}
+        {pendingTx ? 'Waiting...' : 'Set' }
       </Button>
     </div>
+    {pendingTx === 'SENDING' ? 'Sending...' : pendingTx && <div>Waiting for transaction to complete: {pendingTx}</div>}
   </div>
 }
 
