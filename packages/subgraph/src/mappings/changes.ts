d450fa8209831c5ea243b11ff93587959794170b
Brad McDermott <bradmcdermott@gmail.com>
2021-03-16 17:01:31 -0400

    Updates subgraph module to use actual event and contract info

---
+++ packages/subgraph/src/mappings/changes.ts
@@ -0,0 +1,10 @@
+import { Change } from "../types/schema";
+import { Change as ChangeEvent } from "../types/SimpleStorageV1/simpleStorageV1";
+
+export function handleChange(event: ChangeEvent): void {
+  let transactionHash = event.transaction.hash.toHex();
+  let change = new Change(transactionHash);
+  change.message = event.params.message
+  change.newVal = event.params.newVal
+  change.save();
+}
