5790a2e9f430529d3b4e7504673c2d900727ab77
Brad McDermott <bradmcdermott@gmail.com>
2021-03-17 12:30:37 -0400

    Adds block number to subgraph schema for sorting

--- packages/subgraph/src/mappings/changes.ts
+++ packages/subgraph/src/mappings/changes.ts
@@ -2,9 +2,11 @@ import { Change } from "../types/schema";
 import { Change as ChangeEvent } from "../types/SimpleStorageV1/simpleStorageV1";
 
 export function handleChange(event: ChangeEvent): void {
-  let transactionHash = event.transaction.hash.toHex();
-  let change = new Change(transactionHash);
+  let transactionHash = event.transaction.hash.toHex()
+  let blockNumber = event.block.number
+  let change = new Change(transactionHash)
   change.message = event.params.message
   change.newVal = event.params.newVal
+  change.block = blockNumber
   change.save();
 }
